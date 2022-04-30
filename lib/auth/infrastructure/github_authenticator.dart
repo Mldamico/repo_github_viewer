65024c0738e1375df4ac5bb8fd23724cd56abb8e
mldamico <matias.lioneldamico@gmail.com>
2022-04-30 19:38:44 -0300

    Credentials storage and github authenticator

---
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/services.dart';
+import 'package:oauth2/oauth2.dart';
+import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
+
+class GithubAuthenticator {
+  final CredentialsStorage _credentialsStorage;
+
+  GithubAuthenticator(this._credentialsStorage);
+
+  static const scopes = ['read:user', 'repo'];
+  static final authorizationEndpoint =
+      Uri.parse('https://github.com/login/oauth/authorize');
+
+  static final tokenEndpoint =
+      Uri.parse('https://github.com/login/oauth/access_token');
+
+  static final redirectUrl = Uri.parse('http://localhost:3000/callback');
+
+  Future<Credentials?> getSignedInCredentials() async {
+    try {
+      final storedCredentials = await _credentialsStorage.read();
+      if (storedCredentials != null) {
+        if (storedCredentials.canRefresh && storedCredentials.isExpired) {
+          //Todo
+        }
+      }
+      return storedCredentials;
+    } on PlatformException {
+      return null;
+    }
+  }
+
+  Future<bool> isSignedIn() =>
+      getSignedInCredentials().then((credentials) => credentials != null);
+}
